8cf8e402b1f6d7392995282f26e0472a04819286
Andrew Belt <andrewpbelt@gmail.com>
2017-10-08 05:50:53 -0400

    Define VERSION_*, so running `make VERSION=0.4.0` for example defines the macro VERSION_0_4_0

--- compile.mk
+++ compile.mk
@@ -1,5 +1,5 @@
 VERSION ?= dev
-FLAGS += -DVERSION=$(VERSION)
+FLAGS += -DVERSION=$(VERSION) -DVERSION_$(subst .,_,$(VERSION))
 
 # Generate dependency files alongside the object files
 FLAGS += -MMD
