d6ce40984303684c1a2b0f20c88d97baed2e329f
Arnaud Estublier <a.estublier@protonmail.com>
2018-02-21 14:47:28 +0100

    Adding RtAudio Flag so that it does not auto-connect with JACK

--- src/audio.cpp
+++ src/audio.cpp
@@ -149,7 +149,7 @@ void AudioIO::openStream() {
 		inParameters.nChannels = numInputs;
 
 		RtAudio::StreamOptions options;
-		// options.flags |= RTAUDIO_SCHEDULE_REALTIME;
+		options.flags |= RTAUDIO_JACK_DONT_CONNECT;
 
 		int closestSampleRate = deviceInfo.preferredSampleRate;
 		for (int sr : deviceInfo.sampleRates) {
