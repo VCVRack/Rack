accb848ee5c0edd0d8b90f06f02cd194f24a1e2b
Andrew Belt <andrewpbelt@gmail.com>
2018-03-06 16:21:50 -0500

    Make ModuleBrowser centered again

--- src/app/ModuleBrowser.cpp
+++ src/app/ModuleBrowser.cpp
@@ -413,6 +413,7 @@ struct ModuleBrowser : OpaqueWidget {
 	}
 
 	void step() override {
+		box.pos = parent->box.size.minus(box.size).div(2).round();
 		box.pos.y = 60;
 		box.size.y = parent->box.size.y - 2 * box.pos.y;
 
@@ -515,7 +516,6 @@ void appModuleBrowserCreate() {
 
 	ModuleBrowser *moduleBrowser = new ModuleBrowser();
 	overlay->addChild(moduleBrowser);
-	moduleBrowser->box.pos = gMousePos.minus(moduleBrowser->box.getCenter());
 
 	gScene->setOverlay(overlay);
 }
