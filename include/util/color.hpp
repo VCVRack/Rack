fc3a625d41c9836881e01af65229d7fd032df1c6
Andrew Belt <andrewpbelt@gmail.com>
2018-03-18 01:34:19 -0400

    Use screen blending with alpha compositing for LightWidget rendering

--- include/util/color.hpp
+++ include/util/color.hpp
@@ -26,10 +26,32 @@ inline NVGcolor colorPlus(NVGcolor a, NVGcolor b) {
 	return a;
 }
 
+inline NVGcolor colorMult(NVGcolor a, NVGcolor b) {
+	for (int i = 0; i < 3; i++)
+		a.rgba[i] *= b.rgba[i];
+	return a;
+}
+
 inline NVGcolor colorMult(NVGcolor a, float x) {
 	for (int i = 0; i < 3; i++)
 		a.rgba[i] *= x;
 	return a;
+}
+
+/** Screen blending with alpha compositing */
+inline NVGcolor colorScreen(NVGcolor a, NVGcolor b) {
+	if (a.a == 0.0)
+		return b;
+	if (b.a == 0.0)
+		return a;
+
+	a = colorMult(a, a.a);
+	b = colorMult(b, b.a);
+	NVGcolor c = colorMinus(colorPlus(a, b), colorMult(a, b));
+	c.a = a.a + b.a - a.a * b.a;
+	c = colorMult(c, 1.f / c.a);
+	c = colorClip(c);
+	return c;
 }
 
 
--- src/app/MultiLightWidget.cpp
+++ src/app/MultiLightWidget.cpp
@@ -11,11 +11,11 @@ void MultiLightWidget::addBaseColor(NVGcolor baseColor) {
 
 void MultiLightWidget::setValues(const std::vector<float> &values) {
 	assert(values.size() == baseColors.size());
-	color = nvgRGBf(0, 0, 0);
+	color = nvgRGBAf(0, 0, 0, 0);
 	for (size_t i = 0; i < baseColors.size(); i++) {
 		NVGcolor c = baseColors[i];
-		c = colorMult(c, values[i]);
-		color = colorPlus(color, c);
+		c.a *= clamp(values[i], 0.f, 1.f);
+		color = colorScreen(color, c);
 	}
 	color = colorClip(color);
 }
