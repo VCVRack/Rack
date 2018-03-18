e1c2dbc104a825ad1383962d2e3171d358399cb2
Andrew Belt <andrewpbelt@gmail.com>
2018-03-18 00:08:46 -0400

    Fix lastPath, lights WIP

--- src/app/MultiLightWidget.cpp
+++ src/app/MultiLightWidget.cpp
@@ -11,10 +11,9 @@ void MultiLightWidget::addBaseColor(NVGcolor baseColor) {
 
 void MultiLightWidget::setValues(const std::vector<float> &values) {
 	assert(values.size() == baseColors.size());
-	color = bgColor;
+	color = nvgRGBf(0, 0, 0);
 	for (size_t i = 0; i < baseColors.size(); i++) {
 		NVGcolor c = baseColors[i];
-		c = colorMinus(c, bgColor);
 		c = colorMult(c, values[i]);
 		color = colorPlus(color, c);
 	}
