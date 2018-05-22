fa622a502b5a4d4f79c7d73904f15514799aeaad
Andrew Belt <andrewpbelt@gmail.com>
2018-05-22 14:56:16 -0400

    Fixed reset() and toJson() for continuous encoders

--- src/app/Knob.cpp
+++ src/app/Knob.cpp
@@ -7,7 +7,8 @@
 
 namespace rack {
 
-#define KNOB_SENSITIVITY 0.0015
+
+static const float KNOB_SENSITIVITY = 0.0015f;
 
 
 Knob::Knob() {
@@ -26,13 +27,14 @@ void Knob::onDragMove(EventDragMove &e) {
 		range = maxValue - minValue;
 	}
 	else {
-		range = 1.0 - (-1.0);
+		// Continuous encoders scale as if their limits are +/-1
+		range = 1.f - (-1.f);
 	}
 	float delta = KNOB_SENSITIVITY * -e.mouseRel.y * speed * range;
 
 	// Drag slower if Mod is held
 	if (windowIsModPressed())
-		delta /= 16.0;
+		delta /= 16.f;
 	dragValue += delta;
 	dragValue = clamp2(dragValue, minValue, maxValue);
 	if (snap)
