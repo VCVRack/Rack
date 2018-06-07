5548c826c1b25e94dad8d2fd78e4eb3d32f8ace4
Andrew Belt <andrewpbelt@gmail.com>
2018-06-06 23:31:46 -0400

    Add 10 pixels to tooltip because of Retina text width bug

--- src/ui/Tooltip.cpp
+++ src/ui/Tooltip.cpp
@@ -10,7 +10,7 @@ Tooltip::Tooltip() {
 
 void Tooltip::draw(NVGcontext *vg) {
 	// Wrap size to contents
-	box.size.x = bndLabelWidth(vg, -1, text.c_str());
+	box.size.x = bndLabelWidth(vg, -1, text.c_str()) + 10.0;
 	box.size.y = bndLabelHeight(vg, -1, text.c_str(), INFINITY);
 
 	bndTooltipBackground(vg, 0.0, 0.0, box.size.x, box.size.y);
