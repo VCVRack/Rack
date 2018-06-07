550e47096f827c828f5e9abec4dc6b6e7a247e49
Andrew Belt <andrewpbelt@gmail.com>
2018-06-07 09:49:54 -0400

    Use Framebuffer for IconButton SVG

--- src/ui/IconButton.cpp
+++ src/ui/IconButton.cpp
@@ -6,13 +6,19 @@ namespace rack {
 
 IconButton::IconButton() {
 	box.size.x = BND_TOOL_WIDTH;
+
+	fw = new FramebufferWidget();
+	fw->oversample = 2;
+	addChild(fw);
+
 	sw = new SVGWidget();
 	sw->box.pos = Vec(2, 2);
-	addChild(sw);
+	fw->addChild(sw);
 }
 
 void IconButton::setSVG(std::shared_ptr<SVG> svg) {
 	sw->setSVG(svg);
+	fw->dirty = true;
 }
 
 
