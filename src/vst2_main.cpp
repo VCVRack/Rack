633385a5ed4222a3cc3eaa71e702c89a1370eaf7
bsp2 <bs@tkscript.de>
2018-10-28 10:24:20 +0100

    revert host fps fallback

--- src/vst2_main.cpp
+++ src/vst2_main.cpp
@@ -643,14 +643,12 @@ public:
    void setRefreshRate(float _hz) {
       if(_hz < 15.0f)
       {
-#ifdef YAC_LINUX
-         redraw_ival_ms = 1000/30u; // start timer
-#else
          redraw_ival_ms = 0u;
-#endif
       }
       else
+      {
          redraw_ival_ms = sUI(1000.0f / _hz);
+      }
 
       if(b_editor_open)
       {
