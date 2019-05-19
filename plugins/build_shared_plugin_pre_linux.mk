9a883d1411a8427ac1e50a5d77160344de8eb69f
bsp2 <bs@tkscript.de>
2019-05-19 16:51:17 +0200

    add dep/ include folder (for lglw / ZZC plugin)

--- plugins/build_shared_plugin_pre_linux.mk
+++ plugins/build_shared_plugin_pre_linux.mk
@@ -4,7 +4,7 @@ include ../../../../dep/yac/install_linux.mk
 
 TARGET_BASENAME=$(SLUG)
 
-EXTRAFLAGS+= -DVERSION=0.6.1 -D_USE_MATH_DEFINES -DUSE_VST2 -DRACK_PLUGIN -DRACK_PLUGIN_SHARED -DSLUG=$(SLUG) -I../../../../include/ -I../../../../dep/include -Idep/include 
+EXTRAFLAGS+= -DVERSION=0.6.1 -D_USE_MATH_DEFINES -DUSE_VST2 -DRACK_PLUGIN -DRACK_PLUGIN_SHARED -DSLUG=$(SLUG) -I../../../../include/ -I../../../../dep/ -I../../../../dep/include -Idep/include 
 EXTRAFLAGS+=
 
 EXTRALIBS+= ../../../Rack_shared.a
