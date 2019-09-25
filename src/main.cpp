ac5e544d62ce665b7425946718b3154a7e1bbebd
Andrew Belt <andrewpbelt@gmail.com>
2019-09-25 04:40:33 -0400

    Change screenshot flag to -t [zoom]. Re-enable on Mac.

--- src/main.cpp
+++ src/main.cpp
@@ -77,14 +77,12 @@ int main(int argc, char* argv[]) {
 			case 'h': {
 				settings::headless = true;
 			} break;
-#if !defined ARCH_MAC
 			// Due to Mac app translocation and Apple adding a -psn... flag when launched, disable screenshots on Mac for now.
-			case 'p': {
+			case 't': {
 				screenshot = true;
 				// If parsing number failed, use default value
-				sscanf(optarg, "%f", &screenshotZoom);
+				std::sscanf(optarg, "%f", &screenshotZoom);
 			} break;
-#endif
 			case 's': {
 				asset::systemDir = optarg;
 			} break;
