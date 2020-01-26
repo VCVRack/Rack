a41a0e7836a810538980d565b8de961e98605ac8
Andrew Belt <andrewpbelt@gmail.com>
2020-01-26 13:38:09 -0500

    Fix tag comments in tag.cpp.

--- src/tag.cpp
+++ src/tag.cpp
@@ -8,25 +8,25 @@ namespace tag {
 
 
 const std::vector<std::vector<std::string>> tagAliases = {
-	{"Arpeggiator"}, // With a level knob and not much else.
-	{"Attenuator"}, // No parameters or ports. Serves no purpose except visual.
-	{"Blank"},
+	{"Arpeggiator"},
+	{"Attenuator"}, // With a level knob and not much else.
+	{"Blank"}, // No parameters or ports. Serves no purpose except visual.
 	{"Chorus"},
-	{"Clock generator", "Clock"}, // Clock dividers, multipliers, etc.
-	{"Clock modulator"}, // With threshold, ratio, knee, etc parameters.
-	{"Compressor"}, // Use only if the artist "performs" with this module. Simply having knobs is not enough. Examples: on-screen keyboard, XY pad.
-	{"Controller"},
+	{"Clock generator", "Clock"},
+	{"Clock modulator"}, // Clock dividers, multipliers, etc.
+	{"Compressor"}, // With threshold, ratio, knee, etc parameters.
+	{"Controller"}, // Use only if the artist "performs" with this module. Simply having knobs is not enough. Examples: on-screen keyboard, XY pad.
 	{"Delay"},
 	{"Digital"},
 	{"Distortion"},
-	{"Drum", "Drums", "Percussion"}, // The core functionality times two. If multiple channels are a requirement for the module to exist (ring modulator, mixer, etc), it is not a Dual module.
-	{"Dual"},
+	{"Drum", "Drums", "Percussion"},
+	{"Dual"}, // The core functionality times two. If multiple channels are a requirement for the module to exist (ring modulator, mixer, etc), it is not a Dual module.
 	{"Dynamics"},
 	{"Effect"},
 	{"Envelope follower"},
 	{"Envelope generator"},
-	{"Equalizer", "EQ"}, // Expands the functionality of a "mother" module when placed next to it. Expanders should inherit the tags of its mother module.
-	{"Expander"},
+	{"Equalizer", "EQ"},
+	{"Expander"}, // Expands the functionality of a "mother" module when placed next to it. Expanders should inherit the tags of its mother module.
 	{"External"},
 	{"Filter", "VCF", "Voltage controlled filter"},
 	{"Flanger"},
@@ -45,8 +45,8 @@ const std::vector<std::vector<std::string>> tagAliases = {
 	{"Panning", "Pan"},
 	{"Phaser"},
 	{"Physical modeling"},
-	{"Polyphonic", "Poly"}, // The core functionality times four. If multiple channels are a requirement for the module to exist (ring modulator, mixer, etc), it is not a Quad module.
-	{"Quad"},
+	{"Polyphonic", "Poly"},
+	{"Quad"}, // The core functionality times four. If multiple channels are a requirement for the module to exist (ring modulator, mixer, etc), it is not a Quad module.
 	{"Quantizer"},
 	{"Random"},
 	{"Recording"},
@@ -57,10 +57,10 @@ const std::vector<std::vector<std::string>> tagAliases = {
 	{"Sequencer"},
 	{"Slew limiter"},
 	{"Speech"},
-	{"Switch"}, // A synth voice must have, at the minimum, a built-in oscillator and envelope.
-	{"Synth voice"},
-	{"Tuner"}, // Serves only extremely basic functions, like inverting, max, min, multiplying by 2, etc.
-	{"Utility"},
+	{"Switch"},
+	{"Synth voice"}, // A synth voice must have, at the minimum, a built-in oscillator and envelope.
+	{"Tuner"},
+	{"Utility"}, // Serves only extremely basic functions, like inverting, max, min, multiplying by 2, etc.
 	{"Visual"},
 	{"Vocoder"},
 	{"Voltage-controlled amplifier", "Amplifier", "VCA", "Voltage controlled amplifier"},
