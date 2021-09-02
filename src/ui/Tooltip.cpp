80094b0f2eaa3d573f0c21649056a0423508406c
Andrew Belt <andrewpbelt@gmail.com>
2021-09-02 13:21:46 -0400

    Remove right-hand spacing from Tooltip.

--- src/ui/Tooltip.cpp
+++ src/ui/Tooltip.cpp
@@ -11,9 +11,9 @@ void Tooltip::step() {
 	// Wrap size to contents
 	nvgSave(APP->window->vg);
 	nvgTextLineHeight(APP->window->vg, 1.2);
-	box.size.x = bndLabelWidth(APP->window->vg, -1, text.c_str()) + 10.0;
+	box.size.x = bndLabelWidth(APP->window->vg, -1, text.c_str());
 	box.size.y = bndLabelHeight(APP->window->vg, -1, text.c_str(), INFINITY);
-	// Position near cursor. This assumes that `this` is added to the root widget.
+	// Position near cursor. This assumes that the Tooltip is added to the root widget.
 	box.pos = APP->scene->mousePos.plus(math::Vec(15, 15));
 	// Fit inside parent
 	assert(parent);
@@ -26,7 +26,7 @@ void Tooltip::step() {
 void Tooltip::draw(const DrawArgs& args) {
 	bndTooltipBackground(args.vg, 0.0, 0.0, box.size.x, box.size.y);
 	nvgTextLineHeight(args.vg, 1.2);
-	bndMenuLabel(args.vg, 0.0, 0.0, box.size.x, box.size.y, -1, text.c_str());
+	bndMenuLabel(args.vg, 0.0, 0.0, INFINITY, box.size.y, -1, text.c_str());
 	Widget::draw(args);
 }
 
