bdc428dd78fe13d759b5cc6fc8e5e2a77860baf5
Andrew Belt <andrewpbelt@gmail.com>
2022-10-19 17:51:06 -0400

    Replace sse2neon with simde.

--- include/simd/common.hpp
+++ include/simd/common.hpp
@@ -1,9 +1,4 @@
 #pragma once
 
-#if defined ARCH_X64
-	// Intel intrinsics header
-	#include <x86intrin.h>
-#elif defined ARCH_ARM64
-	// Translation header for using SSE3 intrinsics on ARM64 NEON
-	#include <sse2neon.h>
-#endif
+#define SIMDE_ENABLE_NATIVE_ALIASES
+#include <simde/x86/sse3.h>
