a273092bdb6b757c412b287843110f9ecfccb973
Andrew Belt <andrewpbelt@gmail.com>
2022-12-12 15:27:38 -0500

    Use x64 intrinsic header on x64, instead of simde since conflicts with plugins including x64 intrinsic headers.

--- include/simd/common.hpp
+++ include/simd/common.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
-#define SIMDE_ENABLE_NATIVE_ALIASES
-#include <simde/x86/sse4.2.h>
+#ifdef __SSE4_2__
+	#include <nmmintrin.h>
+#else
+	#define SIMDE_ENABLE_NATIVE_ALIASES
+	#include <simde/x86/sse4.2.h>
+#endif
